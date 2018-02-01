5a0ba16740c6c567c5c14ccb4dc79752e2510673
natalia-grechyanik <natalia.grechyanik@vrpinc.com>
2018-02-01 15:25:23 +0300

    added os verification

--- src/test/java/myProject/TestExample.java
+++ src/test/java/myProject/TestExample.java
@@ -14,46 +14,49 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class TestExample {
 
 	public WebDriver driver;
-	
+
 	@BeforeTest
-	public void beforeTest() throws InterruptedException{
-		System.setProperty("webdriver.chrome.driver", "chromedriver-linux");
-		//driver = new ChromeDriver();
-		
-		FirefoxBinary firefoxBinary = new FirefoxBinary();
-		   firefoxBinary.addCommandLineOptions("--headless");
-		   System.setProperty("webdriver.gecko.driver", "geckodriver");
-		   FirefoxOptions firefoxOptions = new FirefoxOptions();
-		   firefoxOptions.setBinary(firefoxBinary);
-		   driver = new FirefoxDriver(firefoxOptions);
-		
+	public void beforeTest() throws InterruptedException {
+
+		String osName = System.getProperty("os.name");
+		System.out.println(osName);
+
+		if (osName == "Mac OS X") {
+			System.setProperty("webdriver.chrome.driver", "chromedriver-linux");
+			driver = new ChromeDriver();
+		} else {
+			FirefoxBinary firefoxBinary = new FirefoxBinary();
+			firefoxBinary.addCommandLineOptions("--headless");
+			System.setProperty("webdriver.gecko.driver", "geckodriver");
+			FirefoxOptions firefoxOptions = new FirefoxOptions();
+			firefoxOptions.setBinary(firefoxBinary);
+			driver = new FirefoxDriver(firefoxOptions);
+		}
 		driver.get("https://login.salesforce.com");
-		
+
 		Thread.sleep(5000);
-		
+
 		WebElement usernameField = driver.findElement(By.id("username"));
 		usernameField.sendKeys("natalia.grechyanik@dev-1.com");
 		WebElement passwordField = driver.findElement(By.id("password"));
 		passwordField.sendKeys("pas5w0rd!");
-		
+
 		WebElement loginButton = driver.findElement(By.id("Login"));
 		loginButton.click();
-		
+
 		Thread.sleep(10000);
 	}
-	
+
 	@Test
-	public void test_1(){
+	public void test_1() {
 		System.out.println("test-1");
-	
-		
+
 	}
-	
+
 	@AfterTest
-	public void afterTest(){
+	public void afterTest() {
 		System.out.println("####");
 		driver.quit();
 	}
-
 
 }
